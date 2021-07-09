be6f72f4b76abf76d3add806ad97a7439a84c20e
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-08 23:09:52 -0300

    fizzbuzz exercise

---
+++ first-chapter/fizzBuzz.js
@@ -0,0 +1,21 @@
+for (let number = 0; number <= 100; number++) {
+    const isNotZero = number !== 0
+
+    const isDivisibleByThree = isNotZero && number % 3 === 0
+    const isDivisibleByFive = isNotZero && number % 5 === 0
+
+    const isDivisibleByThreeAndFive = isDivisibleByThree && isDivisibleByFive
+    const isDivisibleOnlyByThree = isDivisibleByThree && !isDivisibleByFive
+    const isDivisibleOnlyByFive = isDivisibleByFive && !isDivisibleByThree
+
+    if (isDivisibleOnlyByThree) {
+        console.log('Fizz')
+    } else if (isDivisibleOnlyByFive) {
+        console.log('Buzz')
+    } else if (isDivisibleByThreeAndFive) {
+        console.log('FizzBuzz')
+    } 
+    else {
+        console.log(number)
+    }
+}
